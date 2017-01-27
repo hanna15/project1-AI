68d45b87dc49164d676bad6455934a43b713f636
tinnathuridur <tinnath@Tinnas-MacBook-Pro.local>
2017-01-27 17:29:28 +0000

    added Remote Agent class and made some functions to initialize Environment -T&H&H

--- src/Environment.java
+++ src/Environment.java
@@ -1,17 +1,80 @@
 package src;
 
+import java.util.Arrays;
+
 public class Environment {
     public boolean obstacles[][]; //betra að hafa lista, því vitum ekki fyrir fram size?
     public boolean dirt[][];
     public Position home_pos;
     public Orientation home_orient;
     public int num_dirt;
+    public int sizeX;
+    public int sizeY;
+    
+    Environment() {
+        num_dirt = 0;
+    }
+    
+    Environment(int sX, int sY) {
+        setSize(sX, sY);
+        dirt = new boolean[sizeX][sizeY];
+        obstacles = new boolean[sizeX][sizeY];
+        num_dirt = 0;
+    }
     
     Environment(boolean [][] o, boolean[][] d, Position home_p, Orientation home_o, int initial_num_dirt) {
-    	obstacles = o;
-    	dirt = d;
-    	home_pos = home_p;
-    	home_orient = home_o;
-    	num_dirt = initial_num_dirt;
+        obstacles = o;
+        dirt = d;
+        home_pos = home_p;
+        home_orient = home_o;
+        num_dirt = initial_num_dirt;
+    }
+    
+    private void setSize(int x, int y) { // ath, switch?
+        sizeX = x;
+        sizeY = y;
+    }
+    
+    public void setHomePos(int x, int y) {
+        home_pos = new Position(x-1, y-1);
+    }
+    
+    public void setHomeOrient(Orientation o) {
+        home_orient = o;
+    }
+    
+    public void addObstacle(int x, int y) {
+        obstacles[x-1][y-1] = true;
+    }
+    
+    public void addDirt(int x, int y) {
+        dirt[x-1][y-1] = true;
+        num_dirt ++;
+    }
+    
+    
+    public void printEnvironment() {
+        System.out.println("sizeX " + sizeX );
+        System.out.println("sizeY " + sizeY );
+        System.out.println("num_dirt " + num_dirt);
+        
+        for(int i = 0; i < dirt.length; i++) {
+            for (int j = 0; j < dirt[0].length; j++) {
+                System.out.print( " [" + dirt[i][j] + "] ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        for(int i = 0; i < obstacles.length; i++) {
+            for (int j = 0; j < obstacles[0].length; j++) {
+                System.out.print( " [" + obstacles[i][j] + "] " );
+            }
+            System.out.println("");
+        }
+        
+        
+        System.out.println("obstacles " + Arrays.deepToString(obstacles) );
+        System.out.println("home_pos " + home_pos );
+        System.out.println("home_orient " + home_orient );
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,97 +4,97 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
-	
-	/**
-	 * starts the game player and waits for messages from the game master <br>
-	 * Command line options: [port]
-	 */
-	public static void main(String[] args){
-		int W = 10; //width = columns
-		int L = 20; //length = rows
-		boolean my_dirt[][] = new boolean[L][W]; //initializes to false
-		my_dirt[2][3] = true;
-		my_dirt[0][0] = true;
-		
-		boolean my_obstacles[][] = new boolean[L][W];
-
-		Position home_pos = new Position(1,2);
-		Environment e = new Environment(my_obstacles, my_dirt, home_pos, Orientation.NORTH, 2);
-		System.out.println("Home pos is " + e.home_pos);
-		
-		State home_s = new State(new Position(1,2), Orientation.NORTH, true, false);
-		
-		State s = new State(new Position(0,0), Orientation.NORTH, true, true);
-		System.out.println("current state: " + s.contains_dirt + " " + s.position + " " + s.orientation);
-		
-		//Testing successorState function
-		State succ_suck = s.successorState(Action.SUCK);
-		System.out.println("SuccStateSuck- IsDirt?" + succ_suck.contains_dirt);
-		State succ_go = s.successorState(Action.GO);
-		System.out.println("SuccStateGo pos: " + succ_go.position);
-		State succ_left = s.successorState(Action.TURN_LEFT);
-		System.out.println("SuccStateLeft orient: " + succ_left.orientation);
-		State succ_right = s.successorState(Action.TURN_RIGHT);
-		System.out.println("SuccStateRight orient: " + succ_right.orientation);
-		
-		//Testing function "legal actions"
-		ArrayList<Action> actions = s.legalActions(e);
-		if (actions.size() == 0) {
-			System.out.println("No legal acctions");
-		}
-		else {
-			System.out.println("Legal actions are: ");
-			for (Action a : actions) {
-				System.out.print(a + " ");
-			}
-			System.out.println();
-		}
-		
-		ArrayList<Action> actions2 = succ_suck.legalActions(e);
-		if (actions2.size() == 0) {
-			System.out.println("No legal acctions");
-		}
-		else {
-			System.out.println("Legal actions are: ");
-			for (Action a : actions2) {
-				System.out.print(a + " ");
-			}
-			System.out.println();
-		}
-		
-		
-		if(s.isGoalState(e)) {
-			System.out.println("s isGoalState");
-		}
-		else {
-			System.out.println("s is not goal state");
-		}
-		e.num_dirt = 0;
-		State shouldBeGoal = new State(new Position(1,2), Orientation.NORTH, true, false);
-		
-		if(shouldBeGoal.isGoalState(e)) {
-			System.out.println("yeah isGoalState");
-		}
-		else {
-			System.out.println("ohh, it is not goal state");
-		}
-		Node n_root = new Node(home_s);
-		Node n = new Node(succ_suck, n_root, Action.SUCK, 5);
-		System.out.println(n.getPathFromRoot());
-		
-		/*try{
-			// TODO: put in your agent here
-			Agent agent = new RandomAgent();
-
-			int port=4001;
-			if(args.length>=1){
-				port=Integer.parseInt(args[0]);
-			}
-			GamePlayer gp=new GamePlayer(port, agent);
-			gp.waitForExit();
-		}catch(Exception ex){
-			ex.printStackTrace();
-			System.exit(-1);
-		}*/
-	}
+    
+    /**
+     * starts the game player and waits for messages from the game master <br>
+     * Command line options: [port]
+     */
+    public static void main(String[] args){
+        //		int W = 10; //width = columns
+        //		int L = 20; //length = rows
+        //		boolean my_dirt[][] = new boolean[L][W]; //initializes to false
+        //		my_dirt[2][3] = true;
+        //		my_dirt[0][0] = true;
+        //
+        //		boolean my_obstacles[][] = new boolean[L][W];
+        //
+        //		Position home_pos = new Position(1,2);
+        //		Environment e = new Environment(my_obstacles, my_dirt, home_pos, Orientation.NORTH, 2);
+        //		System.out.println("Home pos is " + e.home_pos);
+        //
+        //		State home_s = new State(new Position(1,2), Orientation.NORTH, true, false);
+        //
+        //		State s = new State(new Position(0,0), Orientation.NORTH, true, true);
+        //		System.out.println("current state: " + s.contains_dirt + " " + s.position + " " + s.orientation);
+        //
+        //		//Testing successorState function
+        //		State succ_suck = s.successorState(Action.SUCK);
+        //		System.out.println("SuccStateSuck- IsDirt?" + succ_suck.contains_dirt);
+        //		State succ_go = s.successorState(Action.GO);
+        //		System.out.println("SuccStateGo pos: " + succ_go.position);
+        //		State succ_left = s.successorState(Action.TURN_LEFT);
+        //		System.out.println("SuccStateLeft orient: " + succ_left.orientation);
+        //		State succ_right = s.successorState(Action.TURN_RIGHT);
+        //		System.out.println("SuccStateRight orient: " + succ_right.orientation);
+        //
+        //		//Testing function "legal actions"
+        //		ArrayList<Action> actions = s.legalActions(e);
+        //		if (actions.size() == 0) {
+        //			System.out.println("No legal acctions");
+        //		}
+        //		else {
+        //			System.out.println("Legal actions are: ");
+        //			for (Action a : actions) {
+        //				System.out.print(a + " ");
+        //			}
+        //			System.out.println();
+        //		}
+        //
+        //		ArrayList<Action> actions2 = succ_suck.legalActions(e);
+        //		if (actions2.size() == 0) {
+        //			System.out.println("No legal acctions");
+        //		}
+        //		else {
+        //			System.out.println("Legal actions are: ");
+        //			for (Action a : actions2) {
+        //				System.out.print(a + " ");
+        //			}
+        //			System.out.println();
+        //		}
+        //
+        //
+        //		if(s.isGoalState(e)) {
+        //			System.out.println("s isGoalState");
+        //		}
+        //		else {
+        //			System.out.println("s is not goal state");
+        //		}
+        //		e.num_dirt = 0;
+        //		State shouldBeGoal = new State(new Position(1,2), Orientation.NORTH, true, false);
+        //
+        //		if(shouldBeGoal.isGoalState(e)) {
+        //			System.out.println("yeah isGoalState");
+        //		}
+        //		else {
+        //			System.out.println("ohh, it is not goal state");
+        //		}
+        //		Node n_root = new Node(home_s);
+        //		Node n = new Node(succ_suck, n_root, Action.SUCK, 5);
+        //		System.out.println(n.getPathFromRoot());
+        //		
+        try{
+            // TODO: put in your agent here
+            Agent agent = new RemoteAgent();
+            
+            int port=4001;
+            if(args.length>=1){
+                port=Integer.parseInt(args[0]);
+            }
+            GamePlayer gp=new GamePlayer(port, agent);
+            gp.waitForExit();
+        }catch(Exception ex){
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
